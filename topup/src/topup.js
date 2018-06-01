8dd545066f298d1cab1b86147e57382d594cdd94
Lawrence Wang <l@lw.io>
2018-05-31 19:43:52 -0700

    round addAmount and set a minimum

--- topup/src/topup.js
+++ topup/src/topup.js
@@ -2,18 +2,18 @@ const {
   ConfigFactory,
   Maker
 } = require('@makerdao/makerdao-exchange-integration')
+const roundTo = require('round-to')
+
+const MIN_ADD_AMOUNT = 0.0001
+const ROUND_TO_PLACES = 4
 
 module.exports = async function(cdpId, options) {
   const targetRatio = Number(options.targetRatio)
-  if (isNaN(targetRatio)) throw new Error("Invalid value for targetRatio")
+  if (isNaN(targetRatio)) throw new Error('Invalid value for targetRatio')
 
-  // init maker lib
   const config = ConfigFactory.create('decentralized-oasis-without-proxies')
   config.services.log = 'NullLogger'
   const maker = new Maker(config)
-  await maker.authenticate()
-
-  // retrieve cdp
   const cdp = await maker.getCdp(cdpId)
 
   const collateral = await cdp.getCollateralAmount()
@@ -22,13 +22,20 @@ module.exports = async function(cdpId, options) {
   const debt = await cdp.getDebtAmount()
   console.log(`debt: ${debt} DAI`)
 
-  const collateralPrice = await maker.service('priceFeed').getEthPrice();
+  const collateralPrice = await maker.service('priceFeed').getEthPrice()
   console.log(`ETH/USD: ${collateralPrice}`)
 
   const ratio = collateralPrice * collateral / debt
   if (ratio < targetRatio) {
-    const addAmount =
-      (targetRatio * debt - collateralPrice * collateral) / collateralPrice;
+    let addAmount =
+      (targetRatio * debt - collateralPrice * collateral) / collateralPrice
+
+    if (addAmount < MIN_ADD_AMOUNT) {
+      addAmount = MIN_ADD_AMOUNT
+    } else {
+      addAmount = roundTo.up(addAmount, ROUND_TO_PLACES)
+    }
+
     console.log(`ratio is ${ratio}; adding ${addAmount} ETH.`)
     await cdp.lockEth(addAmount.toString())
   } else {
