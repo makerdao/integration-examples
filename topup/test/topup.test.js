83e5b1f78f8b298cd2fb5c9f5d6cc046c849a65e
Lawrence Wang <l@lw.io>
2018-07-06 11:56:50 -0700

    remove false-negative test

--- topup/test/topup.test.js
+++ topup/test/topup.test.js
@@ -1,15 +1,13 @@
 const topup = require('../src/topup');
 
-test("should not throw an error when accessing maker.js", async () => {
-  expect(async () => {
-    await topup(26, { targetRatio: 2.5 });
-  }).not.toThrow();
-});
-
-test("should use the library to define necessary values", async () => {
-  const values = await topup(26, { targetRatio: 2.5 });
-  expect.assertions(values.length);
-  for (i = 0; i < values.length; i++) {
-    expect(values[i]).toBeDefined();
-  }
-});
+test(
+  'should use the library to define necessary values',
+  async () => {
+    const values = await topup(26, { targetRatio: 2.5 });
+    expect.assertions(values.length);
+    for (i = 0; i < values.length; i++) {
+      expect(values[i]).toBeDefined();
+    }
+  },
+  15000
+);
