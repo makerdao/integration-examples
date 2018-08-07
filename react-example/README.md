c701e21d250ece12eeb55a8ab04cb52a3877b14a
Lawrence Wang <l@lw.io>
2018-08-06 17:53:12 -0700

    update library dep for react-example

--- react-example/README.md
+++ react-example/README.md
@@ -1,5 +1,5 @@
 ### Exchange integration example
 
-This is a basic React app (created with `create-react-app`) that imports `@makerdao/exchange-integration-poc`, initializes a new `Maker` object, and uses it for basic CDP functionality.
+This is a basic React app (created with `create-react-app`) that imports `@makerdao/dai`, initializes a new `Maker` object, and uses it for basic CDP functionality.
 
 The only relevant code is in `src/App.js`, unless you want to run it yourself, in which case you can simply clone this repo, `npm install`, and `npm start`.
--- react-example/src/actions/index.js
+++ react-example/src/actions/index.js
@@ -1,73 +1,81 @@
-import Maker from '@makerdao/makerdao-exchange-integration';
+import Maker from '@makerdao/dai';
 
 export const started = () => ({
   type: 'STARTED'
-})
+});
 
 export const makerCreated = () => ({
   type: 'MAKER_CREATED'
-})
+});
 
 export const cdpOpened = () => ({
   type: 'CDP_OPENED'
-})
+});
 
 export const ethLocked = () => ({
   type: 'ETH_LOCKED'
-})
+});
 
 export const daiDrawn = () => ({
   type: 'DAI_DRAWN'
-})
+});
 
 export const daiWiped = () => ({
   type: 'DAI_WIPED'
-})
+});
 
 export const cdpShut = () => ({
   type: 'CDP_SHUT'
-})
+});
 
 const drawDaiAsync = (maker, cdp) => async dispatch => {
-  const defaultAccount = maker.service('token').get('web3').defaultAccount();
+  const defaultAccount = maker
+    .service('token')
+    .get('web3')
+    .defaultAccount();
   const dai = maker.service('token').getToken('DAI');
-  const txn = await cdp.drawDai(.1);
+  const txn = await cdp.drawDai(0.1);
   const balance = await dai.balanceOf(defaultAccount);
   console.log('Transaction from drawing Dai:', txn);
   console.log('Dai balance after drawing:', balance.toString());
   dispatch(daiDrawn());
-}
+};
 
 const wipeDebtAsync = (maker, cdp) => async dispatch => {
-  const defaultAccount = maker.service('token').get('web3').defaultAccount();
+  const defaultAccount = maker
+    .service('token')
+    .get('web3')
+    .defaultAccount();
   const dai = maker.service('token').getToken('DAI');
-  const txn = await cdp.wipeDai(.1);
+  const txn = await cdp.wipeDai(0.1);
   const balance = await dai.balanceOf(defaultAccount);
 
   console.log('Transaction from wiping Dai:', txn);
   console.log('Dai balance after wiping:', balance.toString());
   dispatch(daiWiped());
-}
+};
 
-const shutCdpAsync = (cdp) => async dispatch => {
+const shutCdpAsync = cdp => async dispatch => {
   const txn = await cdp.shut();
   console.log('Transaction from shutting the CDP:', txn);
   dispatch(cdpShut());
-}
-
+};
 
 export const startAsync = () => async dispatch => {
-    dispatch(started());
-    const maker = new Maker('kovan', { privateKey: process.env.REACT_APP_PRIVATE_KEY, overrideMetamask: true });
-    console.log('maker:', maker);
-    dispatch(makerCreated());
-    const cdp = await maker.openCdp();
-    console.log('cdp:', cdp);
-    dispatch(cdpOpened());
-    const lockEthTx = await cdp.lockEth(0.01);
-    console.log('transaction to lock eth:', lockEthTx);
-    dispatch(ethLocked());
-    await dispatch(drawDaiAsync(maker, cdp));
-    await dispatch(wipeDebtAsync(maker,cdp));
-    await dispatch(shutCdpAsync(cdp));
-}
+  dispatch(started());
+  const maker = new Maker('kovan', {
+    privateKey: process.env.REACT_APP_PRIVATE_KEY,
+    overrideMetamask: true
+  });
+  console.log('maker:', maker);
+  dispatch(makerCreated());
+  const cdp = await maker.openCdp();
+  console.log('cdp:', cdp);
+  dispatch(cdpOpened());
+  const lockEthTx = await cdp.lockEth(0.01);
+  console.log('transaction to lock eth:', lockEthTx);
+  dispatch(ethLocked());
+  await dispatch(drawDaiAsync(maker, cdp));
+  await dispatch(wipeDebtAsync(maker, cdp));
+  await dispatch(shutCdpAsync(cdp));
+};
